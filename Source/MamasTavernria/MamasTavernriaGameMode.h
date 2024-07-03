e852685d1671fd64d31dda58b4763451b2bed8dc
Quinn72 <90585060+Quinn72@users.noreply.github.com>
2024-07-03 10:13:04 +1000

    say sorry again i dare you

---
+++ Source/MamasTavernria/MamasTavernriaGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MamasTavernriaGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMamasTavernriaGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMamasTavernriaGameMode();
+};
+
+
+
